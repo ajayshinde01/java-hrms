d2d5ae96000d53e4bfb1fc7e53a109b3fe0d2e4f
ronak <ronak.hariya@avisys.in>
2023-07-31 16:18:59 +0530

    Added Datatable metadata Controller

---
+++ src/main/java/com/avisys/empmgmt/dto/ColumnMetadata.java
@@ -0,0 +1,50 @@
+package com.avisys.empmgmt.dto;
+/**
+ * This will contain column metadata such as name of the column, which object
+ * value it should be mapped with, the type of column as provided by
+ * {@link ColumnType} and the width of column.
+ */
+public class ColumnMetadata {
+	private String name;
+	private String mappedBy;
+	private String sortBy;
+	private ColumnType type;
+	private int width;
+
+	/**
+	 * Construct an instance of ColumnMetadata
+	 * 
+	 * @param name     name of the column to be displayed on UI
+	 * @param mappedBy what field or attribute it should look at from the response
+	 *                 to map that column's value
+	 * @param type     type of the column. See {@link ColumnType}
+	 * @param width    width of the column in percentage without the % sign
+	 */
+	public ColumnMetadata(String name, String mappedBy,String sortBy, ColumnType type, int width) {
+		this.name = name;
+		this.mappedBy = mappedBy;
+		this.sortBy=sortBy;
+		this.type = type;
+		this.width = width;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getMappedBy() {
+		return mappedBy;
+	}
+
+	public ColumnType getType() {
+		return type;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public String getSortBy() {
+		return sortBy;
+	}
+}
