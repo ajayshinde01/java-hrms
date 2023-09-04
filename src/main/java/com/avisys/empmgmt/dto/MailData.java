17f07d1b14e456566968a915c8c95246131123bf
AKSHAY <akshay.deshmukh@avisys.in>
2023-09-04 15:32:42 +0530

    added email functionality

--- src/main/java/com/avisys/empmgmt/dto/MailData.java
+++ src/main/java/com/avisys/empmgmt/dto/MailData.java
@@ -7,12 +7,45 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class MailData {
 	private long id;
 	private String email;
 	private Map<String, String> data;
+	
+	public MailData() {
+		super();
+	}
+
+	public MailData(long id, String email, Map<String, String> data) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.data = data;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Map<String, String> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, String> data) {
+		this.data = data;
+	}
+	
+	
 }
